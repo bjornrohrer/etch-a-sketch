76ce95856dc0c94254e3301659fe6f29946b0710
bjornrohrer <barohrerdk@gmail.com>
2024-08-13 13:34:11 +0200

    Added the size of the sketchArea

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 const gridSide = 600;
 let rows = 16;
 let cols = 16;
+
+const sketchArea = document.querySelector("#sketch-area");
+sketchArea.style.width = `${sketchArea}px`;
+sketchArea.style.height = `${sketchArea}px`;
