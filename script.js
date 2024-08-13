cb5e51273ff998f0de5dd20d890b4c97a73905be
bjornrohrer <barohrerdk@gmail.com>
2024-08-13 13:37:48 +0200

    Create a function and put a for loop inside the function.

--- script.js
+++ script.js
@@ -5,3 +5,7 @@ let cols = 16;
 const sketchArea = document.querySelector("#sketch-area");
 sketchArea.style.width = `${sketchArea}px`;
 sketchArea.style.height = `${sketchArea}px`;
+
+function createGridCells() {
+    for (let i = 0; i < (rows * cols); i++)
+}
