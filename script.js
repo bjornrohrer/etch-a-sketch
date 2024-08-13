9ab4da3344368d54205a121ed91b473173d70712
bjornrohrer <barohrerdk@gmail.com>
2024-08-13 14:45:40 +0200

    Added variable for slider.

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const sketchArea = document.querySelector("#sketch-area");
 sketchArea.style.width = `${sketchArea}px`;
 sketchArea.style.height = `${sketchArea}px`;
 
+const sliderContainer = document.querySelector("#slider-container");
+
 function setBackgroundColor() {
   this.style.backgroundColor = "black";
 }
